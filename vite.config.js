a5f7c005f4d2c1495d29a5f01594b2ccf00b82bb
qqw <774132019@qq.com>
2024-05-23 23:17:27 +0800

    后端gateway地址更换

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig(({ mode, command }) => {
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'http://localhost:8080',
+          target: 'http://39.101.184.20:18080',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
